0ac8b2c7e963d9c56f95f9e82f6283f1a12cae34
Sithum Imarsha <sithum.imarshadev@gmail.com>
2025-01-10 18:50:47 +0530

    feat(client): add  populer product summary for the dashboard

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,5 +1,13 @@
+"use client";
+
+import CardPopularProducts from "./CardPopularProducts";
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <CardPopularProducts />
+    </div>
+  );
 };
 
 export default Page;
